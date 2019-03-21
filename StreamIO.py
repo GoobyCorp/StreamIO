ae3729f92bc6535220e870223fb656b1098a7339
John <pls@gooby.co>
2019-03-21 17:10:38 -0400

    Added a default stream for bytes and bytearrays

--- StreamIO.py
+++ StreamIO.py
@@ -71,6 +71,8 @@ class StreamIO(object):
             self.stream = BytesIO()
         else:
             self.stream = stream
+        if isinstance(self.stream, bytes) or isinstance(self.stream, bytearray):
+            self.stream = BytesIO(self.stream)
         self.can_seek = self.stream.seekable()
         self.can_tell = self.stream.seekable()
 
